d4aefd0bf6bdd0ce84e06f0022994d16f0a622fb
Iryna Savchuk <iryna.o.savchuk@gmail.com>
2020-10-16 21:00:26 +0300

    Added Data Visualization

---
+++ exploratory-data-visualization/Line Charts-142.py
@@ -0,0 +1,42 @@
+## 2. Introduction To The Data ##
+
+import pandas as pd
+
+unrate = pd.read_csv("unrate.csv")
+unrate['DATE'] = pd.to_datetime(unrate['DATE'])
+
+print(unrate.head(12))
+
+## 6. Introduction to Matplotlib ##
+
+import matplotlib.pyplot as plt
+plt.plot()
+plt.show()
+
+## 7. Adding Data ##
+
+import pandas as pd
+
+unrate = pd.read_csv("unrate.csv")
+unrate['DATE'] = pd.to_datetime(unrate['DATE'])
+
+df = unrate.head(12)
+plt.plot(df["DATE"], df["VALUE"])
+plt.show()
+
+## 8. Fixing Axis Ticks ##
+
+df = unrate.head(12)
+plt.plot(df["DATE"], df["VALUE"])
+plt.xticks(rotation=90)
+plt.show()
+
+## 9. Adding Axis Labels And A Title ##
+
+df = unrate.head(12)
+plt.plot(df["DATE"], df["VALUE"])
+plt.xticks(rotation=90)
+plt.xlabel("Month")
+plt.ylabel("Unemployment Rate")
+plt.title("Monthly Unemployment Trends, 1948")
+plt.show()
